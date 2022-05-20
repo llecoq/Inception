3df455e18a747e6e1ef8161e60add7c9c2962500
llecoq <llecoq@student.42lyon.fr>
2022-05-20 15:24:03 +0200

    nginx: new user created so the container doesn't run as root

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -10,18 +10,14 @@ state=Auvergne-Rhone-Alpes
 locality=Lyon
 organization="42 Lyon"
 organizationalunit=IT
-email=llecoq@student.42lyon.fr
-
-# Optional
-# password=mypassword
 
 # For this project self-signed certificate is required but it is not recommended in production
 
 # Generate self-signed certificate that is valid only 7 days everytime the server is launched
-mkdir /etc/ssl/private
+# mkdir /etc/ssl/private
 openssl req -newkey rsa:2048 -x509 -sha256 -days 7 -nodes \
 	-keyout /etc/ssl/private/$domain.key \
 	-out /etc/ssl/certs/$domain.crt \
-	-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+	-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname"
 
 exec "$@"
