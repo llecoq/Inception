10164a9d27738a27d64167e13cd8c5388178af2b
llecoq <llecoq@student.42lyon.fr>
2022-05-23 16:43:44 +0200

    nginx: nginx user and group created but container not running as nginx user anymore. The server is already running as nginx user

--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -14,7 +14,6 @@ organizationalunit=IT
 # For this project self-signed certificate is required but it is not recommended in production
 
 # Generate self-signed certificate that is valid only 7 days everytime the server is launched
-# mkdir /etc/ssl/private
 openssl req -newkey rsa:2048 -x509 -sha256 -days 7 -nodes \
 	-keyout /etc/ssl/private/$domain.key \
 	-out /etc/ssl/certs/$domain.crt \
