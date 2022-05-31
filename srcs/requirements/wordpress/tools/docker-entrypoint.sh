0ac1e8927a1e37fcf2a4e9d598d5b94a95943f2c
llecoq <llecoq@student.42lyon.fr>
2022-05-31 17:56:14 +0200

    nginx is now linked with wordpress php files via php-fpm

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,1 +1,7 @@
-echo "ah"
+# sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+# chown -R www-data:www-data /var/www/*;
+# chown -R 755 /var/www/*;
+# mkdir -p /run/php/;
+# touch /run/php/php7.3-fpm.pid;
+
+exec "$@"
