d91be8326e72b1d97115c0152321bc63079e801e
llecoq <llecoq@student.42lyon.fr>
2022-06-02 13:58:18 +0200

    wordress: security modification in the entrypoint script

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -9,12 +9,19 @@ mv wp-cli.phar /usr/local/bin/wp
 echo "... wp-cli is ready to use !"
 
 # bash script for checking whether WordPress is installed or not 
-if ! wp core is-installed --path=${WORDPRESS_PATH} 1>&2;
+if ! wp core is-installed --path=${WORDPRESS_PATH};
 	then
 		# download WordPress core
 		echo "WordPress is not installed yet. Downloading wp core..."
 		wp core download --path=${WORDPRESS_PATH}
-	
+
+		# checking if the database is ready and reachable
+		until mysql --host=mariadb --user=${DB_USER} --password=${DB_PASSWORD} -e "exit";
+			do
+				echo "Database is not ready yet."
+				sleep 1
+		done
+
 		# create wp-config.php that will link WordPress to the database
 		echo "Creating a new wp-config.php with database constants, and verifying that the database constants are correct..."
 		wp config create --dbname=${DB_NAME} \
