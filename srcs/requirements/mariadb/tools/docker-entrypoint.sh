1f6355072b607927a5c8df298f68ff7a5790b36e
llecoq <llecoq@student.42lyon.fr>
2022-06-02 13:33:21 +0200

    Wordpress && mariadb: Minor changes to the scripts for better clarity

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-set -ex
 
 # Set bind-address so MySQL can bind to all networks and make sure skip-networking is off
+echo "Setting up mariadb-server configuration file so it can bind to remote network..."
 sed -ie 's/#bind-address/bind-address/g' /etc/my.cnf.d/mariadb-server.cnf
 sed -ie 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+echo "... Success !"
 
 # set up and run new database if it doesn't already exist in the shared volume
 # else start server via Dockerfile CMD
 if [ ! -d /var/lib/mysql/${DB_NAME} ];
 	then
+		echo "Database does not exist yet."
 		# install/initialize MySQL database / equivalent of /etc/init.d/mariadb setup
 		mysql_install_db --user=${DB_USER} --datadir=/var/lib/mysql
 		# start mariadb server
@@ -16,7 +18,7 @@ if [ ! -d /var/lib/mysql/${DB_NAME} ];
 		# secure installation of mariadb / same steps as mariadb-secure-installation script :
 		# set database root password, delete anonymous user, ensure the root user can not log in remotely,
 		# remove the test database and flush the privileges tables.
-		# Creating new database and granting privileges to the DB user;
+		# Creating new database and granting privileges to the DB user with renote access;
 		mysql --user=root <<_EOF_
 		ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 		DELETE FROM mysql.user WHERE User='';
@@ -28,6 +30,10 @@ if [ ! -d /var/lib/mysql/${DB_NAME} ];
 		FLUSH PRIVILEGES;
 _EOF_
 		mysqladmin --user=root --password=$DB_ROOT_PASSWORD shutdown
+	else
+		echo "Database is already installed and configured !"
 fi
+
+echo "Starting up mariadb's server..."
 
 exec "$@"
--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
 
 # download wp-cli command line tool for interacting with and managing WordPress sites.
+echo "Downloading WordPress command line tool..."
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # make it an executable file and move it to the local /bin
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
+echo "... wp-cli is ready to use !"
 
-# if WordPress isn't already installed and configured : 
-if [ ! -e /var/www/html/wp-config.php ]
+# bash script for checking whether WordPress is installed or not 
+if ! wp core is-installed --path=${WORDPRESS_PATH} 1>&2;
 	then
 		# download WordPress core
+		echo "WordPress is not installed yet. Downloading wp core..."
 		wp core download --path=${WORDPRESS_PATH}
+	
 		# create wp-config.php that will link WordPress to the database
+		echo "Creating a new wp-config.php with database constants, and verifying that the database constants are correct..."
 		wp config create --dbname=${DB_NAME} \
 						 --dbuser=${DB_USER} \
 						 --dbpass=${DB_PASSWORD} \
 						 --dbhost=${DB_HOST} \
 						 --path=${WORDPRESS_PATH}
 		# create admin user
-		wp core install --url=${DOMAIN_NAME} \
-						--title=${PROJECT_NAME} \
-						--admin_user=${ADMIN_USER} \
-						--admin_password=${ADMIN_PASSWORD} \
-						--admin_email=${ADMIN_EMAIL} \
-						--path=${WORDPRESS_PATH}
+		echo "Creating the WordPress tables in the database using the URL, title, and default admin user details provided..."
+		wp core install  --url=${DOMAIN_NAME} \
+						 --title=${PROJECT_NAME} \
+						 --admin_user=${ADMIN_USER} \
+						 --admin_password=${ADMIN_PASSWORD} \
+						 --admin_email=${ADMIN_EMAIL} \
+						 --skip-email \
+						 --path=${WORDPRESS_PATH}
+	else
+		echo "WordPress is already installed and properly configured !"
 fi
 
+echo "Starting up FastCGI Process Manager..."
 exec "$@"
